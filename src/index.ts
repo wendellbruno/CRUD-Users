9d6ef61e4cd71796141f82b13609544d3ad6ff5e
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2024-02-09 10:25:08 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hellou')
+import express from 'express';
+import {config} from 'dotenv'
+
+config()
+
+const app = express();
+
+const port = process.env.PORT || 8001
+
+app.listen(port, () => console.log('app rodando na porta', port));
+
+app.get('/', (req,res) => {
+    res.send('olá')
+})
