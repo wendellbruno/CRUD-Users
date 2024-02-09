5b60dbc8d2c57be87da2371629668a3acb45d2a7
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2024-02-09 10:43:27 -0300

    feat: add getUsersController
    feat: add getUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,22 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+import { Request, Response } from "express";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (erro) {
+      return {
+        statusCode: 500,
+        body: "erro",
+      };
+    }
+  }
+}
---
+++ src/model/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    fistName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
